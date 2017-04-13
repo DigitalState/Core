b7e64555e89dd3c8fe861b11a7885de54958fa37
mario-digitalstate <mario@digitalstate.ca>
2017-04-13 07:13:30 -0400

    Update authorization context builder to be based off configs

---
+++ src/Ds/Component/Security/Serializer/ContextBuilder/AuthorizationContextBuilder.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Ds\Component\Security\Serializer\ContextBuilder;
+
+use ApiPlatform\Core\Serializer\SerializerContextBuilderInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class AuthorizationContextBuilder
+ */
+class AuthorizationContextBuilder implements SerializerContextBuilderInterface
+{
+    /**
+     * @var string
+     */
+    protected $entity;
+
+    /**
+     * @var array
+     */
+    protected $groups;
+
+    /**
+     * @var \ApiPlatform\Core\Serializer\SerializerContextBuilderInterface
+     */
+    protected $decorated;
+
+    /**
+     * @var \Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface
+     */
+    protected $authorizationChecker;
+
+    /**
+     * Constructor
+     *
+     * @param string $entity
+     * @param array $context
+     * @param \ApiPlatform\Core\Serializer\SerializerContextBuilderInterface $decorated
+     * @param \Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface $authorizationChecker
+     */
+    public function __construct($entity, array $groups, SerializerContextBuilderInterface $decorated, AuthorizationCheckerInterface $authorizationChecker)
+    {
+        $this->entity = $entity;
+        $this->groups = $groups;
+        $this->decorated = $decorated;
+        $this->authorizationChecker = $authorizationChecker;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createFromRequest(Request $request, bool $normalization, array $extractedAttributes = null) : array
+    {
+        $context = $this->decorated->createFromRequest($request, $normalization, $extractedAttributes);
+        $data = $request->attributes->get('data');
+
+        if (!$data instanceof $this->entity) {
+            return $context;
+        }
+
+        foreach ($this->groups as $role => $groups) {
+            if (!$this->authorizationChecker->isGranted($role)) {
+                continue;
+            }
+
+            $context['groups'] = array_merge($context['groups'], $groups[($normalization ? '' : 'de').'normalization']);
+        }
+
+        return $context;
+    }
+}
