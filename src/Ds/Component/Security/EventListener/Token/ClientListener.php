55581c4642f03a55fbd98dec33dcbc98f0a7839f
mario-digitalstate <mario@digitalstate.ca>
2017-04-16 17:11:05 -0400

    Add client token listener

---
+++ src/Ds/Component/Security/EventListener/Token/ClientListener.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Ds\Component\Security\EventListener\Token;
+
+use Symfony\Component\HttpFoundation\RequestStack;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTDecodedEvent;
+
+/**
+ * Class ClientListener
+ */
+class ClientListener
+{
+    /**
+     * @var \Symfony\Component\HttpFoundation\RequestStack
+     */
+    private $requestStack;
+
+    /**
+     * Constructor
+     *
+     * @param \Symfony\Component\HttpFoundation\RequestStack $requestStack
+     */
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
+    /**
+     * On created
+     *
+     * @param \Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent $event
+     */
+    public function onCreated(JWTCreatedEvent $event)
+    {
+        $request = $this->requestStack->getCurrentRequest();
+        $payload = $event->getData();
+        $payload['client'] = md5($request->server->get('HTTP_USER_AGENT'));
+        $event->setData($payload);
+    }
+
+    /**
+     * On decoded
+     *
+     * @param \Lexik\Bundle\JWTAuthenticationBundle\Event\JWTDecodedEvent $event
+     */
+    public function onDecoded(JWTDecodedEvent $event)
+    {
+        $payload = $event->getPayload();
+
+        if (!array_key_exists('client', $payload)) {
+            $event->markAsInvalid();
+        }
+    }
+}
