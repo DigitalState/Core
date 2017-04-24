382145f05346d4f3c91fdd3293d898ecaf970085
mario-digitalstate <mario@digitalstate.ca>
2017-04-24 19:57:22 -0400

    Add entity component

---
+++ src/Ds/Component/Entity/Entity/Association.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace Ds\Component\Entity\Entity;
+
+use Ds\Component\Model\Type\Identifiable;
+use Ds\Component\Model\Type\Uuidentifiable;
+use Ds\Component\Model\Type\Associable;
+use Ds\Component\Model\Type\Ownable;
+use Ds\Component\Model\Accessor;
+use Knp\DoctrineBehaviors\Model As Behavior;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiProperty;
+use Symfony\Component\Serializer\Annotation As Serializer;
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints as ORMAssert;
+
+/**
+ * Class Association
+ *
+ * @ApiResource(
+ *      attributes={
+ *          "normalization_context"={"groups"={"association_output"}},
+ *          "denormalization_context"={"groups"={"association_input"}}
+ *      }
+ * )
+ * @ORM\Entity(repositoryClass="Ds\Component\Entity\Repository\AssociationRepository")
+ * @ORM\Table(name="ds_association")
+ * @ORM\HasLifecycleCallbacks
+ * @ORMAssert\UniqueEntity(fields="uuid")
+ */
+class Association implements Identifiable, Uuidentifiable, Associable, Ownable
+{
+    use Behavior\Timestampable\Timestampable;
+    use Behavior\SoftDeletable\SoftDeletable;
+
+    use Accessor\Id;
+    use Accessor\Uuid;
+    use Accessor\Entity;
+    use Accessor\EntityUuid;
+    use Accessor\Owner;
+    use Accessor\OwnerUuid;
+
+    /**
+     * @var integer
+     * @ApiProperty(identifier=false, writable=false)
+     * @Serializer\Groups({"association_output"})
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(name="id", type="integer")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     * @ApiProperty(identifier=true, writable=false)
+     * @Serializer\Groups({"association_output"})
+     * @ORM\Column(name="uuid", type="guid", unique=true)
+     * @Assert\Uuid
+     */
+    protected $uuid;
+
+    /**
+     * @var \DateTime
+     * @ApiProperty(writable=false)
+     * @Serializer\Groups({"association_output"})
+     */
+    protected $createdAt;
+
+    /**
+     * @var \DateTime
+     * @ApiProperty(writable=false)
+     * @Serializer\Groups({"association_output"})
+     */
+    protected $updatedAt;
+
+    /**
+     * @var \DateTime
+     * @ApiProperty(writable=false)
+     * @Serializer\Groups({"association_output"})
+     */
+    protected $deletedAt;
+
+    /**
+     * @var string
+     * @ApiProperty
+     * @Serializer\Groups({"association_output", "association_input"})
+     * @ORM\Column(name="entity", type="string")
+     * @Assert\NotBlank
+     */
+    protected $entity;
+
+    /**
+     * @var string
+     * @ApiProperty
+     * @Serializer\Groups({"association_output", "association_input"})
+     * @ORM\Column(name="entity_uuid", type="guid")
+     * @Assert\NotBlank
+     * @Assert\Uuid
+     */
+    protected $entityUuid;
+
+    /**
+     * @var string
+     * @ApiProperty
+     * @Serializer\Groups({"association_output", "association_input"})
+     * @ORM\Column(name="`owner`", type="string")
+     * @Assert\NotBlank
+     */
+    protected $owner;
+
+    /**
+     * @var string
+     * @ApiProperty
+     * @Serializer\Groups({"association_output", "association_input"})
+     * @ORM\Column(name="owner_uuid", type="guid")
+     * @Assert\NotBlank
+     * @Assert\Uuid
+     */
+    protected $ownerUuid;
+}
---
+++ src/Ds/Component/Entity/Repository/AssociationRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Ds\Component\Entity\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Class AssociationRepository
+ */
+class AssociationRepository extends EntityRepository
+{
+}
