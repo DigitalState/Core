4bf4db64cb37c7020f8574d973ecf6b90bc2f6c1
mario <mario@digitalstate.ca>
2017-05-28 11:46:55 -0400

    Add config collection

---
+++ src/Ds/Component/Config/Collection/ConfigCollection.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Ds\Component\Config\Collection;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Class ConfigCollection
+ */
+class ConfigCollection extends ArrayCollection
+{
+}
