cf82791ad4843c0d5f9c0737e2e782e0c8a8cc90
mario <mario@digitalstate.ca>
2017-06-12 22:34:48 -0400

    Add value model accessor

---
+++ src/Ds/Component/Model/Accessor/Value.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Ds\Component\Model\Accessor;
+
+/**
+ * Trait Value
+ */
+trait Value
+{
+    /**
+     * Set value
+     *
+     * @param mixed $value
+     * @return object
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * Get value
+     *
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
