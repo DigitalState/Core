bb85f7c502b5ff6c4795e59e493baddd183f821c
mario <mario@digitalstate.ca>
2017-06-26 19:44:49 -0400

    Restructure translation filters

--- src/Ds/Component/Translation/Doctrine/ORM/Filter/OrderFilter.php
+++ src/Ds/Component/Translation/Doctrine/ORM/Filter/OrderFilter.php
@@ -2,49 +2,48 @@
 
 namespace Ds\Component\Translation\Doctrine\ORM\Filter;
 
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter as BaseOrderFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use Doctrine\ORM\QueryBuilder;
+use DomainException;
 
 /**
  * Class OrderFilter
  */
-class OrderFilter extends TranslationFilter
+class OrderFilter extends BaseOrderFilter
 {
+    use Translation;
+
+    /**
+     * @const string
+     */
+    const ORDER_ASC = 'ASC';
+    const ORDER_DESC = 'DESC';
+
     /**
      * Filter property
      *
      * @param string $property
-     * @param mixed $value
+     * @param mixed $direction
      * @param \Doctrine\ORM\QueryBuilder $queryBuilder
      * @param \ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface $queryNameGenerator
      * @param string $resourceClass
      * @param string|null $operationName
+     * @throws \DomainException
      */
-    protected function filterProperty(string $property, $value, QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
+    protected function filterProperty(string $property, $direction, QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
     {
-    }
-
-    /**
-     * Get description
-     *
-     * @param string $resourceClass
-     * @return array
-     */
-    public function getDescription(string $resourceClass): array
-    {
-        $description = [];
-
-        foreach ($this->properties as $property => $strategy) {
-            $description['order['.$property.']'] = [
-                'property' => $property,
-                'type' => 'string',
-                'required' => false,
-                'swagger' => [
-                    'description' => ''
-                ],
-            ];
+        if (!$this->isPropertyEnabled($property)) {
+            return;
         }
 
-        return $description;
+        $alias = $this->addJoinTranslation($queryBuilder, $resourceClass);
+        $direction = strtoupper($direction);
+
+        if (!in_array($direction, [static::ORDER_ASC, static::ORDER_DESC], true)) {
+            throw new DomainException('Filter strategy does not exist.');
+        }
+
+        $queryBuilder->addOrderBy(sprintf('%s.%s', $alias, $property), $direction);
     }
 }
--- src/Ds/Component/Translation/Doctrine/ORM/Filter/SearchFilter.php
+++ src/Ds/Component/Translation/Doctrine/ORM/Filter/SearchFilter.php
@@ -2,15 +2,17 @@
 
 namespace Ds\Component\Translation\Doctrine\ORM\Filter;
 
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter as BaseSearchFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use Doctrine\ORM\QueryBuilder;
-use DomainException;
 
 /**
  * Class SearchFilter
  */
-class SearchFilter extends TranslationFilter
+class SearchFilter extends BaseSearchFilter
 {
+    use Translation;
+
     /**
      * Filter property
      *
@@ -20,68 +22,23 @@ class SearchFilter extends TranslationFilter
      * @param \ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface $queryNameGenerator
      * @param string $resourceClass
      * @param string|null $operationName
+     * @throws \DomainException
      */
     protected function filterProperty(string $property, $value, QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
     {
-        if (!array_key_exists($property, $this->properties)) {
+        if (!$this->isPropertyEnabled($property)) {
             return;
         }
 
-        $this->joinTranslation($queryBuilder, $resourceClass);
-        $translationAlias = $this->getTranslationAlias($queryBuilder);
-        $name = $queryNameGenerator->generateParameterName($property);
+        $strategy = $this->properties[$property] ?? self::STRATEGY_EXACT;
+        $alias = $this->addJoinTranslation($queryBuilder, $resourceClass);
+        $caseSensitive = true;
 
-        switch ($this->properties[$property]) {
-            case 'partial':
-                $value = '%'.$value.'%';
-                break;
-
-            case 'start':
-                $value = $value.'%';
-                break;
-
-            case 'end':
-                $value = '%'.$value;
-                break;
-
-            case 'word_start':
-                break;
-
-            case 'exact':
-            case null:
-                $value = $value;
-                break;
-
-            default:
-                throw new DomainException('Filter strategy does not exist.');
+        if (strpos($strategy, 'i') === 0) {
+            $strategy = substr($strategy, 1);
+            $caseSensitive = false;
         }
 
-        $queryBuilder
-            ->andWhere(sprintf('%s.%s LIKE :%s', $translationAlias, $property, $name))
-            ->setParameter($name, $value);
-    }
-
-    /**
-     * Get description
-     *
-     * @param string $resourceClass
-     * @return array
-     */
-    public function getDescription(string $resourceClass): array
-    {
-        $description = [];
-
-        foreach ($this->properties as $property => $strategy) {
-            $description[$property] = [
-                'property' => $property,
-                'type' => 'string',
-                'required' => false,
-                'swagger' => [
-                    'description' => ''
-                ],
-            ];
-        }
-
-        return $description;
+        $this->addWhereByStrategy($strategy, $queryBuilder, $queryNameGenerator, $alias, $property, $value, $caseSensitive);
     }
 }
