edd2e4509417e097ccacb0873b9f9f24de84015a
mario <mario@digitalstate.ca>
2017-07-01 14:45:25 -0400

    Restructure container component

---
+++ src/Ds/Component/Container/Attribute/Accessor/Container.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Ds\Component\Container\Attribute\Accessor;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Trait Container
+ */
+trait Container
+{
+    /**
+     * Set container
+     *
+     * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+     * @return object
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+
+        return $this;
+    }
+
+    /**
+     * Get container
+     *
+     * @return \Symfony\Component\DependencyInjection\ContainerInterface
+     */
+    public function getContainer()
+    {
+        return $this->container;
+    }
+}
--- src/Ds/Component/Container/Attribute/Container.php
+++ src/Ds/Component/Container/Attribute/Container.php
@@ -2,26 +2,15 @@
 
 namespace Ds\Component\Container\Attribute;
 
-use Symfony\Component\DependencyInjection\ContainerInterface;
-
 /**
  * Trait Container
  */
 trait Container
 {
+    use Accessor\Container;
+
     /**
      * @var \Symfony\Component\DependencyInjection\ContainerInterface
      */
-    protected $container; # region accessors
-
-    /**
-     * Set container
-     * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
-     */
-    public function setContainer(ContainerInterface $container = null)
-    {
-        $this->container = $container;
-    }
-
-    # endregion
+    protected $container;
 }
