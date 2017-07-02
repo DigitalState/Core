f367825eccbf6086be3cf7edd22241f129ee12ff
mario <mario@digitalstate.ca>
2017-07-02 14:21:55 -0400

    Restructure translation component config files

--- src/Ds/Component/Translation/Bridge/Symfony/Bundle/DependencyInjection/DsTranslationExtension.php
+++ src/Ds/Component/Translation/Bridge/Symfony/Bundle/DependencyInjection/DsTranslationExtension.php
@@ -21,6 +21,7 @@ class DsTranslationExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $loader->load('parameters.yml');
+        $loader->load('doctrine.yml');
     }
 }
