0d6bc09e4ffd7636aa09245884e092642dda5567
mario <mario@digitalstate.ca>
2017-07-02 14:18:24 -0400

    Restructure model component config files

--- src/Ds/Component/Model/Bridge/Symfony/Bundle/DependencyInjection/DsModelExtension.php
+++ src/Ds/Component/Model/Bridge/Symfony/Bundle/DependencyInjection/DsModelExtension.php
@@ -37,7 +37,8 @@ class DsModelExtension extends Extension implements PrependExtensionInterface
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $loader->load('parameters.yml');
+        $loader->load('event_listeners.yml');
 
         $this->loadBehavior($config['behavior'] ?? [], $container);
     }
