c56839817575dfcbe67f4d53a24049ef0917903e
mario <mario@digitalstate.ca>
2017-07-16 17:29:18 -0400

    Add health component

---
+++ src/Ds/Component/Health/Bridge/Symfony/Bundle/DependencyInjection/Configuration.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Ds\Component\Health\Bridge\Symfony\Bundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * Class Configuration
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $builder = new TreeBuilder;
+        $node = $builder->root('ds_health');
+
+        return $builder;
+    }
+}
---
+++ src/Ds/Component/Health/Bridge/Symfony/Bundle/DsHealthBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Ds\Component\Health\Bridge\Symfony\Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class DsHealthBundle
+ */
+class DsHealthBundle extends Bundle
+{
+}
