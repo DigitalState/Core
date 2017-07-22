c257ba3a536154fe8791996ae8bab7fa506c8d43
mario <mario@digitalstate.ca>
2017-07-22 12:31:54 -0400

    Remove unneeded code

--- src/Ds/Component/Config/Bridge/Symfony/Bundle/DependencyInjection/DsConfigExtension.php
+++ src/Ds/Component/Config/Bridge/Symfony/Bundle/DependencyInjection/DsConfigExtension.php
@@ -27,9 +27,6 @@ class DsConfigExtension extends Extension implements PrependExtensionInterface
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration;
-        $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('parameters.yml');
         $loader->load('api_filters.yml');
