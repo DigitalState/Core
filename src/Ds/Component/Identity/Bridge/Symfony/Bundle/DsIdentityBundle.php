ece6d71262854ecfefe63d05ebef464150ee9753
mario <mario@digitalstate.ca>
2017-07-22 11:46:16 -0400

    Add identity bundle

---
+++ src/Ds/Component/Identity/Bridge/Symfony/Bundle/DsIdentityBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Ds\Component\Identity\Bridge\Symfony\Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class DsIdentityBundle
+ */
+class DsIdentityBundle extends Bundle
+{
+}
