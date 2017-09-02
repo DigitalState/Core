7fcaa45718c62183facda2d83f070676685b4c58
mario <mario@digitalstate.ca>
2017-09-02 10:11:27 -0400

    Further develop formio component

--- src/Ds/Component/Formio/Bridge/Symfony/Bundle/DependencyInjection/DsFormioExtension.php
+++ src/Ds/Component/Formio/Bridge/Symfony/Bundle/DependencyInjection/DsFormioExtension.php
@@ -21,6 +21,7 @@ class DsFormioExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('parameters.yml');
         $loader->load('services.yml');
     }
 }
