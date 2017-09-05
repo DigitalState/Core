72ea9eb58fffd703f4fb673eda6a0a0ba384c407
mario <mario@digitalstate.ca>
2017-09-05 19:04:03 -0400

    Add setter to model id attribute

--- src/Ds/Component/Model/Attribute/Accessor/Id.php
+++ src/Ds/Component/Model/Attribute/Accessor/Id.php
@@ -10,6 +10,19 @@ namespace Ds\Component\Model\Attribute\Accessor;
 trait Id
 {
     /**
+     * Set id
+     *
+     * @param integer $id
+     * @return object
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
      * Get id
      *
      * @return integer
