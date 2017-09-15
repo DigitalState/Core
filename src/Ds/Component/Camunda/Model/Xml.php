4421e9c26ddd218cf2d068651c3fb3f279fdb5ac
mario <mario@digitalstate.ca>
2017-09-14 22:17:40 -0400

    Further develop camunda component

---
+++ src/Ds/Component/Camunda/Model/Xml.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ds\Component\Camunda\Model;
+
+/**
+ * Class Xml
+ *
+ * @package Ds\Component\Camunda
+ */
+class Xml implements Model
+{
+    use Attribute\Id;
+    use Attribute\Xml;
+}
