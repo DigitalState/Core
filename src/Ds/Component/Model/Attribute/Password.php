b0f6abe99a43eafef0f70b1536f4be7551cd2a75
mario <mario@digitalstate.ca>
2017-10-27 15:32:18 -0400

    Add password model attribute and accessor

---
+++ src/Ds/Component/Model/Attribute/Password.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Ds\Component\Model\Attribute;
+
+/**
+ * Trait Password
+ *
+ * @package Ds\Component\Model
+ */
+trait Password
+{
+    use Accessor\Password;
+
+    /**
+     * @var string
+     */
+    protected $password;
+}
