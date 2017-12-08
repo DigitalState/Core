efb70a07727e40da298a23a0ce1cfbca3f8d3379
mario <mario@digitalstate.ca>
2017-12-08 13:29:12 -0500

    Expose audit endpoints

--- src/Ds/Component/Audit/Bridge/Symfony/Bundle/DependencyInjection/DsAuditExtension.php
+++ src/Ds/Component/Audit/Bridge/Symfony/Bundle/DependencyInjection/DsAuditExtension.php
@@ -36,10 +36,18 @@ class DsAuditExtension extends Extension implements PrependExtensionInterface
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $configuration = new Configuration;
+        $config = $this->processConfiguration($configuration, $configs);
+
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('parameters.yml');
+        $loader->load('api_filters.yml');
         $loader->load('event_listeners.yml');
         $loader->load('repositories.yml');
         $loader->load('services.yml');
+
+        // @todo Move this config -> parameters logic to a common trait in the config component bridge
+        $container->setParameter('ds_config.configs.ds_audit.audit.owner', $config['audit']['owner']);
+        $container->setParameter('ds_config.configs.ds_audit.audit.owner_uuid', $config['audit']['owner_uuid']);
     }
 }
