808adfe851207d38245d3890edaf62cf4df8fdde
mario <mario@digitalstate.ca>
2018-02-19 11:49:58 -0500

    Fix bug related to fosuser uppercasing roles.

--- src/Ds/Component/Security/Service/AccessService.php
+++ src/Ds/Component/Security/Service/AccessService.php
@@ -51,8 +51,9 @@ class AccessService extends EntityService
         $roles = [];
 
         foreach ($user->getRoles() as $role) {
-            if (preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/', $role)) {
-                $roles[] = $role;
+            if (preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i', $role)) {
+                // @todo FosUser is uppercasing roles, look into disable that on the authentication microservice.
+                $roles[] = strtolower($role);
             }
         }
 
