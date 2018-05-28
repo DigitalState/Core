e32adb3e39553ec1137313016475e83a17a29b2c
mario <mario@digitalstate.ca>
2018-05-28 09:57:19 -0400

    Restructure api component tenant initializer

--- src/Ds/Component/Api/Tenant/Initializer/ConfigInitializer.php
+++ src/Ds/Component/Api/Tenant/Initializer/ConfigInitializer.php
@@ -32,26 +32,44 @@ class ConfigInitializer implements Initializer
      */
     public function initialize(array $data)
     {
-        $configs = [
-            ['key' => 'ds_api.user.username', 'value' => 'system@system.ds'],
-            ['key' => 'ds_api.user.password', 'value' => $data['user']['system']['password']],
-            ['key' => 'ds_api.user.uuid', 'value' => $data['user']['system']['uuid']],
-            ['key' => 'ds_api.user.roles', 'value' => 'ROLE_SYSTEM'],
-            ['key' => 'ds_api.user.identity', 'value' => 'System'],
-            ['key' => 'ds_api.user.identity_uuid', 'value' => $data['identity']['system']['uuid']]
+        $items = [
+            [
+                'key' => 'ds_api.user.username',
+                'value' => 'system@system.ds'
+            ],
+            [
+                'key' => 'ds_api.user.password',
+                'value' => $data['user']['system']['password']
+            ],
+            [
+                'key' => 'ds_api.user.uuid',
+                'value' => $data['user']['system']['uuid']
+            ],
+            [
+                'key' => 'ds_api.user.roles',
+                'value' => 'ROLE_SYSTEM'
+            ],
+            [
+                'key' => 'ds_api.user.identity',
+                'value' => 'System'
+            ],
+            [
+                'key' => 'ds_api.user.identity_uuid',
+                'value' => $data['identity']['system']['uuid']
+            ]
         ];
 
         $manager = $this->configService->getManager();
 
-        foreach ($configs as $config) {
-            $entity = $this->configService->createInstance();
-            $entity
+        foreach ($items as $item) {
+            $config = $this->configService->createInstance();
+            $config
                 ->setOwner('BusinessUnit')
                 ->setOwnerUuid($data['business_unit']['administration']['uuid'])
-                ->setKey($config['key'])
-                ->setValue($config['value'])
+                ->setKey($item['key'])
+                ->setValue($item['value'])
                 ->setTenant($data['tenant']['uuid']);
-            $manager->persist($entity);
+            $manager->persist($config);
             $manager->flush();
         }
     }
