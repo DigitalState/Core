4049c45cb83dae442ed500769b3698f75fc5bed1
mario <mario@digitalstate.ca>
2018-06-12 08:08:43 -0400

    Further develop formio component

--- src/Ds/Component/Formio/Model/Project.php
+++ src/Ds/Component/Formio/Model/Project.php
@@ -2,6 +2,8 @@
 
 namespace Ds\Component\Formio\Model;
 
+use stdClass;
+
 /**
  * Class Project
  *
@@ -10,4 +12,17 @@ namespace Ds\Component\Formio\Model;
 class Project implements Model
 {
     use Attribute\Id;
+    use Attribute\Title;
+    use Attribute\Name;
+    use Attribute\Description;
+    use Attribute\Template;
+    use Attribute\Settings;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->settings = new stdClass;
+    }
 }
