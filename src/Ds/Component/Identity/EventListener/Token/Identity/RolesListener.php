94d6d7ccdaafca78119873ea4d0368c5495e16bc
mario <mario@digitalstate.ca>
2018-06-13 13:58:08 -0400

    Add synchronously identity roles to jwt token

--- src/Ds/Component/Identity/EventListener/Token/Identity/RolesListener.php
+++ src/Ds/Component/Identity/EventListener/Token/Identity/RolesListener.php
@@ -2,6 +2,10 @@
 
 namespace Ds\Component\Identity\EventListener\Token\Identity;
 
+use AppBundle\Entity\User;
+use DomainException;
+use Ds\Component\Api\Api\Api;
+use Ds\Component\Identity\Model\Identity;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTDecodedEvent;
 use Symfony\Component\PropertyAccess\PropertyAccess;
@@ -13,6 +17,11 @@ use Symfony\Component\PropertyAccess\PropertyAccess;
  */
 class RolesListener
 {
+    /**
+     * @var \Ds\Component\Api\Api\Api
+     */
+    protected $api;
+
     /**
      * @var \Symfony\Component\PropertyAccess\PropertyAccessor
      */
@@ -26,10 +35,12 @@ class RolesListener
     /**
      * Constructor
      *
+     * @param \Ds\Component\Api\Api\Api $api
      * @param string $property
      */
-    public function __construct($property = '[identity][roles]')
+    public function __construct(Api $api, $property = '[identity][roles]')
     {
+        $this->api = $api;
         $this->accessor = PropertyAccess::createPropertyAccessor();
         $this->property = $property;
     }
@@ -44,7 +55,44 @@ class RolesListener
     {
         $data = $event->getData();
         $user = $event->getUser();
-        $this->accessor->setValue($data, $this->property, []);
+        $roles = [];
+
+        // @todo remove condition when both user types are homogenized
+        if ($user instanceof User) {
+            switch ($user->getIdentity()) {
+                case Identity::ANONYMOUS:
+                    $identity = $this->api->get('identities.anonymous')->get($user->getIdentityUuid());
+                    break;
+
+                case Identity::INDIVIDUAL:
+                    $identity = $this->api->get('identities.individual')->get($user->getIdentityUuid());
+                    break;
+
+                case Identity::ORGANIZATION:
+                    $identity = $this->api->get('identities.organization')->get($user->getIdentityUuid());
+                    break;
+
+                case Identity::STAFF:
+                    $identity = $this->api->get('identities.staff')->get($user->getIdentityUuid());
+                    break;
+
+                case Identity::SYSTEM:
+                    $identity = $this->api->get('identities.system')->get($user->getIdentityUuid());
+                    break;
+
+                default:
+                    throw new DomainException('User identity is not valid.');
+            }
+
+            foreach ($identity->getRoles() as $role) {
+                // @todo Remove substr once we remove iri-based api foreign keys
+                $roles[] = substr($role, -36);
+            }
+        } else {
+            $roles = $user->getIdentity()->getRoles();
+        }
+
+        $this->accessor->setValue($data, $this->property, $roles);
         $event->setData($data);
     }
 
