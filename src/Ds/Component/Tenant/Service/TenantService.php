a648a79cecd79e2b4287694bb525c6b1fa2a21b9
mario <mario@digitalstate.ca>
2018-06-18 12:58:40 -0400

    Add transaction rollback to tenant load function

--- src/Ds/Component/Tenant/Service/TenantService.php
+++ src/Ds/Component/Tenant/Service/TenantService.php
@@ -2,9 +2,11 @@
 
 namespace Ds\Component\Tenant\Service;
 
+use Doctrine\ORM\EntityManager;
 use Ds\Component\Config\Service\ParameterService;
 use Ds\Component\Security\Model\User;
 use Ds\Component\Tenant\Collection\LoaderCollection;
+use Exception;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
@@ -15,6 +17,11 @@ use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInt
  */
 class TenantService
 {
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    protected $entityManager;
+
     /**
      * @var \Ds\Component\Config\Service\ParameterService
      */
@@ -38,13 +45,15 @@ class TenantService
     /**
      * Constructor
      *
+     * @param \Doctrine\ORM\EntityManager $entityManager
      * @param \Ds\Component\Config\Service\ParameterService $parameterService
      * @param \Symfony\Component\HttpFoundation\RequestStack $requestStack
      * @param \Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface $tokenStorage
      * @param \Ds\Component\Tenant\Collection\LoaderCollection $loaderCollection
      */
-    public function __construct(ParameterService $parameterService, RequestStack $requestStack, TokenStorageInterface $tokenStorage, LoaderCollection $loaderCollection)
+    public function __construct(EntityManager $entityManager, ParameterService $parameterService, RequestStack $requestStack, TokenStorageInterface $tokenStorage, LoaderCollection $loaderCollection)
     {
+        $this->entityManager = $entityManager;
         $this->parameterService = $parameterService;
         $this->requestStack = $requestStack;
         $this->tokenStorage = $tokenStorage;
@@ -85,12 +94,61 @@ class TenantService
      *
      * @param array $data
      * @return \Ds\Component\Tenant\Service\TenantService
-     * @throws \InvalidArgumentException
+     * @throws \Exception
+     * @example
+     * <code>
+     * [
+     *     'user' => [
+     *         'system' => [
+     *             'uuid' => 'd9e66aeb-d853-41bc-ad9e-ae6fb18fb067',
+     *             'password' => 'password'
+     *         ],
+     *         'admin' => [
+     *             'uuid' => '28de3e41-87c1-47c9-813d-07a87278fc00',
+     *             'password' => 'password'
+     *         ]
+     *     ],
+     *     'identity' => [
+     *         'system' => [
+     *             'uuid' => '782637a1-7366-4805-a853-869f65fa3965'
+     *         ],
+     *         'admin' => [
+     *             'uuid' => '782637a1-7366-4805-a853-869f65fa3965'
+     *         ]
+     *     ],
+     *     'business_unit' => [
+     *         'administration' => [
+     *             'uuid' => '5675406b-f2ed-453a-b3ef-5be4596e7da6'
+     *         ]
+     *     ],
+     *     'tenant' => [
+     *         'uuid' => '10e287ef-fcd0-45e1-b178-b44be9f2363a'
+     *     ],
+     *     'config' => [
+     *         'app.spa.admin' => [
+     *             'value' => 'admin.ds'
+     *         ],
+     *         'app.spa.portal' => [
+     *             'value' => 'portal.ds'
+     *         ]
+     *     ]
+     * ]
+     * </code>
      */
     public function load(array $data)
     {
-        foreach ($this->loaderCollection as $loader) {
-            $loader->load($data);
+        $connection = $this->entityManager->getConnection();
+        $connection->beginTransaction();
+
+        try {
+            foreach ($this->loaderCollection as $loader) {
+                $loader->load($data);
+            }
+
+            $connection->commit();
+        } catch (Exception $exception) {
+            $connection->rollBack();
+            throw $exception;
         }
 
         return $this;
