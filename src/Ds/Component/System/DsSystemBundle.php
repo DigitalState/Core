d5c851b2c035f33e21d870702e0634df36ae6b9e
mario <mario@digitalstate.ca>
2018-06-21 09:19:54 -0400

    Add system component

---
+++ src/Ds/Component/System/DsSystemBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ds\Component\System;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class DsSystemBundle
+ *
+ * @package Ds\Component\System
+ */
+class DsSystemBundle extends Bundle
+{
+}
