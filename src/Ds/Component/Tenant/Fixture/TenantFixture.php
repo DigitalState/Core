4d6329d915494e72ec13cd8537b86331ebdca2b5
mario <mario@digitalstate.ca>
2018-06-21 10:10:45 -0400

    Add tenant fixture

---
+++ src/Ds/Component/Tenant/Fixture/TenantFixture.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Ds\Component\Tenant\Fixture;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Ds\Component\Tenant\Entity\Tenant;
+use Ds\Component\Database\Fixture\ResourceFixture;
+use Ds\Component\Tenant\EventListener\Entity\TenantListener;
+
+/**
+ * Class TenantFixture
+ *
+ * @package Ds\Component\Tenant
+ */
+abstract class TenantFixture extends ResourceFixture
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        // Disable the event listener in charge of loading tenant data
+        $metadata = $manager->getClassMetadata(Tenant::class);
+
+        foreach ($metadata->entityListeners as $event => $listeners) {
+            foreach ($listeners as $key => $listener) {
+                if (TenantListener::class === $listener['class']) {
+                    unset($metadata->entityListeners[$event][$key]);
+                }
+            }
+        }
+
+        $objects = $this->parse($this->getResource());
+
+        foreach ($objects as $object) {
+            $tenant = new Tenant;
+            $tenant
+                ->setUuid($object->uuid)
+                ->setData((array) $object->data);
+            $manager->persist($tenant);
+            $manager->flush();
+        }
+    }
+}
