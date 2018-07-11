21c634757c994d827f0af66dcb62c718d6b5904e
mario-digitalstate <mario@digitalstate.ca>
2018-07-11 07:14:30 -0400

    Add model attributes and accessors

---
+++ src/Ds/Component/Model/Attribute/Token.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Ds\Component\Model\Attribute;
+
+/**
+ * Trait Token
+ *
+ * @package Ds\Component\Model
+ */
+trait Token
+{
+    use Accessor\Token;
+
+    /**
+     * @var string
+     */
+    protected $token;
+}
