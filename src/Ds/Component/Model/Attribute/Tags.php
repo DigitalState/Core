c98340176d17f508544f04b5bcd075f8fac0c356
mario-digitalstate <mario@digitalstate.ca>
2018-08-15 12:13:39 -0400

    Add tags model attribute and accessors

---
+++ src/Ds/Component/Model/Attribute/Tags.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Ds\Component\Model\Attribute;
+
+/**
+ * Trait Tags
+ *
+ * @package Ds\Component\Model
+ */
+trait Tags
+{
+    use Accessor\Tags;
+
+    /**
+     * @var array
+     */
+    protected $tags;
+}
