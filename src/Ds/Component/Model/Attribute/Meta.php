a18ca7dfa7fe09304d5b1079bc0064d1bbd48752
mario-digitalstate <mario@digitalstate.ca>
2018-09-12 14:14:33 -0400

    Add meta property to discovery service model

---
+++ src/Ds/Component/Model/Attribute/Meta.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Ds\Component\Model\Attribute;
+
+/**
+ * Trait Meta
+ *
+ * @package Ds\Component\Model
+ */
+trait Meta
+{
+    use Accessor\Meta;
+
+    /**
+     * @var array
+     */
+    protected $meta;
+}
