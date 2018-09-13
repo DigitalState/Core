04112cb5e1b72c40de687450b2ed1496ee391a9c
Mario Prud'homme <mario@prudhom.me>
2018-09-13 10:39:27 -0400

    Merge feature/system behat context (#54)

---
+++ src/Ds/Component/System/Test/Collection/UserCollection.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ds\Component\System\Test\Collection;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Class UserCollection
+ *
+ * @package Ds\Component\System
+ */
+class UserCollection extends ArrayCollection
+{
+}
---
+++ src/Ds/Component/System/Test/DsSystemTestBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ds\Component\System\Test;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class DsSystemTestBundle
+ *
+ * @package Ds\Component\System
+ */
+class DsSystemTestBundle extends Bundle
+{
+}
