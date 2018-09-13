4e8d803d4569d923f355a61746e668b66ccf3762
mario-digitalstate <mario@digitalstate.ca>
2018-09-13 10:33:11 -0400

    Add behat system context

---
+++ src/Ds/Component/System/Test/DependencyInjection/DsSystemTestExtension.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Ds\Component\System\Test\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+/**
+ * Class DsSystemTestExtension
+ *
+ * @package Ds\Component\System
+ */
+class DsSystemTestExtension extends Extension implements PrependExtensionInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        $container->prependExtensionConfig('ds_system_test', [
+            'users' => []
+        ]);
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('config.yml');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('parameters.yml');
+        $loader->load('collections.yml');
+
+        $configuration = new Configuration;
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $definition = $container->findDefinition('ds_system_test.collection.user');
+        $definition->setArguments([$config['users']]);
+    }
+}
