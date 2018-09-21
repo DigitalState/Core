4e09220ab8164d618160afda93c71a2dbf73f2b7
mario-digitalstate <mario@digitalstate.ca>
2018-09-21 14:02:29 -0400

    Fix syntax

--- src/Ds/Component/Bpm/DependencyInjection/DsBpmExtension.php
+++ src/Ds/Component/Bpm/DependencyInjection/DsBpmExtension.php
@@ -24,5 +24,6 @@ class DsBpmExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('parameters.yml');
-        $loader->load('resolvers.yml');}
+        $loader->load('resolvers.yml');
+    }
 }
