df363d621b85a503e491912243e7a254a1120e27
mario-digitalstate <mario@digitalstate.ca>
2018-10-04 09:35:45 -0400

    Refactor tenant loader listeners to be compatible with encryption component

--- src/Ds/Component/Tenant/EventListener/LoaderListener.php
+++ src/Ds/Component/Tenant/EventListener/LoaderListener.php
@@ -1,23 +1,23 @@
 <?php
 
-namespace Ds\Component\Tenant\EventListener\Entity;
+namespace Ds\Component\Tenant\EventListener;
 
 use Ds\Component\Container\EventListener\ContainerListener;
 use Ds\Component\Tenant\Entity\Tenant;
 
 /**
- * Class TenantListener
+ * Class LoaderListener
  *
  * @package Ds\Component\Tenant
  */
-class TenantListener extends ContainerListener
+class LoaderListener extends ContainerListener
 {
     /**
-     * Load related data when a system tenant is created
+     * Load related data when a system tenant is persisted
      *
      * @param \Ds\Component\Tenant\Entity\Tenant $tenant
      */
-    public function postPersist(Tenant $tenant)
+    public function prePersist(Tenant $tenant)
     {
         $this->container->get('ds_tenant.service.tenant')->load($tenant);
     }
