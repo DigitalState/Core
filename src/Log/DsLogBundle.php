cb6d2b357f51fa2663bb16beeca5612be256cbc9
mario-digitalstate <mario@digitalstate.ca>
2018-11-01 13:01:24 -0400

    Add log component

---
+++ src/Log/DsLogBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ds\Component\Log;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class DsLogBundle
+ *
+ * @package Ds\Component\Log
+ */
+final class DsLogBundle extends Bundle
+{
+}
---
+++ src/Log/Monolog/Processor/AppProcessor.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Ds\Component\Log\Monolog\Processor;
+
+/**
+ * Class AppProcessor
+ *
+ * @package Ds\Component\Log
+ */
+final class AppProcessor
+{
+    /**
+     * @var string
+     */
+    protected $app;
+
+    /**
+     * Constructor
+     *
+     * @param string $app
+     */
+    public function __construct(?string $app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * Process record
+     *
+     * @param array $record
+     * @return array
+     */
+    public function process(array $record): array
+    {
+        if (null !== $this->app) {
+            $record['app'] = $this->app;
+        }
+
+        return $record;
+    }
+}
