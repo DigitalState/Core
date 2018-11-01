86c2eb9f5b2b0e64db00680d3e83c2df77b305a9
mario-digitalstate <mario@digitalstate.ca>
2018-11-01 14:21:35 -0400

    Add debug component

---
+++ src/Debug/DsDebugBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ds\Component\Debug;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class DsDebugBundle
+ *
+ * @package Ds\Component\Debug
+ */
+final class DsDebugBundle extends Bundle
+{
+}
