6774cbc725ac702731676d4ad1b72eae1175f210
mario-digitalstate <mario@digitalstate.ca>
2018-11-09 13:18:36 -0500

    Add exception component

---
+++ src/Exception/DependencyInjection/DsExceptionExtension.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Ds\Component\Exception\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+/**
+ * Class DsExceptionExtension
+ *
+ * @package Ds\Component\Exception
+ */
+final class DsExceptionExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
---
+++ src/Exception/DsExceptionBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ds\Component\Exception;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class DsExceptionBundle
+ *
+ * @package Ds\Component\Exception
+ */
+final class DsExceptionBundle extends Bundle
+{
+}
