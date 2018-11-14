6960881da7d2e5c02a5abc20726b416d83ec712d
mario-digitalstate <mario@digitalstate.ca>
2018-11-14 11:39:27 -0500

    Add association component

---
+++ src/Association/Repository/AssociationRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ds\Component\Association\Repository;
+
+use Ds\Component\Entity\Repository\EntityRepository;
+
+/**
+ * Class AssociationRepository
+ *
+ * @package Ds\Component\Association
+ */
+class AssociationRepository extends EntityRepository
+{
+}
