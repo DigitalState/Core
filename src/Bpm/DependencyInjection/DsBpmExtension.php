52f1e4a7bb1117772fa7d1948a7b1a527325381d
mario-digitalstate <mario@digitalstate.ca>
2018-11-14 12:27:40 -0500

    Add bpm component

---
+++ src/Bpm/DependencyInjection/DsBpmExtension.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Ds\Component\Bpm\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\Config\FileLocator;
+
+/**
+ * Class DsBpmExtension
+ *
+ * @package Ds\Component\Bpm
+ */
+final class DsBpmExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yaml');
+    }
+}
