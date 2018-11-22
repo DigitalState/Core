0f415445fc01385dc2a144184ad56962c4054ffa
mario-digitalstate <mario@digitalstate.ca>
2018-11-22 15:03:08 -0500

    Add metadatas to metadata migration

--- src/Metadata/Migrations/Version0_15_0.php
+++ src/Metadata/Migrations/Version0_15_0.php
@@ -14,13 +14,21 @@ final class Version0_15_0 extends AbstractMigration
      * Up migration
      *
      * @param \Doctrine\DBAL\Schema\Schema $schema
+     * @param array $metadatas
      */
-    public function up(Schema $schema)
+    public function up(Schema $schema, array $metadatas = [])
     {
+        $sequences['ds_metadata_id_seq'] = 1 + count($metadatas);
+        $sequences['ds_metadata_trans_id_seq'] = 1;
+
+        foreach ($metadatas as $metadata) {
+            $sequences['ds_metadata_trans_id_seq'] += count($metadata->title);
+        }
+
         switch ($this->platform->getName()) {
             case 'postgresql':
-                $this->addSql('CREATE SEQUENCE ds_metadata_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-                $this->addSql('CREATE SEQUENCE ds_metadata_trans_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+                $this->addSql('CREATE SEQUENCE ds_metadata_id_seq INCREMENT BY 1 MINVALUE 1 START '.$sequences['ds_metadata_id_seq']);
+                $this->addSql('CREATE SEQUENCE ds_metadata_trans_id_seq INCREMENT BY 1 MINVALUE 1 START '.$sequences['ds_metadata_trans_id_seq']);
                 $this->addSql('CREATE TABLE ds_metadata (id INT NOT NULL, uuid UUID NOT NULL, "owner" VARCHAR(255) DEFAULT NULL, owner_uuid UUID DEFAULT NULL, slug VARCHAR(255) NOT NULL, type VARCHAR(255) DEFAULT NULL, data JSON NOT NULL, version INT DEFAULT 1 NOT NULL, tenant UUID NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
                 $this->addSql('CREATE UNIQUE INDEX UNIQ_11290F17D17F50A6 ON ds_metadata (uuid)');
                 $this->addSql('CREATE UNIQUE INDEX UNIQ_11290F17989D9B624E59C462 ON ds_metadata (slug, tenant)');
