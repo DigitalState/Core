0b06410f5959d6bbcf5a0623aa0ee3ea1cfe0e3c
mario-digitalstate <mario@digitalstate.ca>
2019-04-28 10:56:00 -0400

    Add query component

---
+++ src/Query/DependencyInjection/DsQueryExtension.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Ds\Component\Query\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\Config\FileLocator;
+
+/**
+ * Class DsQueryExtension
+ *
+ * @package Ds\Component\Query
+ */
+final class DsQueryExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yaml');
+    }
+}
